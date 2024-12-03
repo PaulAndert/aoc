2f6401cf91fbc6120ce9b27d922bdbbfa4ffbd08
Paul Andert <paulandert@mail.de>
2024-12-03 21:31:25 +0100

    day 2 a

---
+++ src/y2024/day_02_a.rs
@@ -0,0 +1,34 @@
+use std::fs;
+
+pub fn main() {
+    let contents = fs::read_to_string("./src/y2024/resources/day_02").expect("Should have been able to read the file");
+
+    let mut matrix: Vec<Vec<i32>> = Vec::new();
+    for line in contents.split("\n") {
+        let lists: Vec<&str> = line.split(" ").collect::<Vec<&str>>();
+        if lists.len() > 1 {
+            let numbers: Vec<i32> = lists.iter().map(|a| a.parse::<i32>().unwrap()).collect();
+            matrix.push(numbers);
+        }
+    }
+
+    let mut count = 0;
+    for report in matrix {
+        if evaluate_report(report) {
+            count += 1
+        }
+    }
+    
+    println!("C: {}", count);
+}
+
+fn evaluate_report(report: Vec<i32>) -> bool {
+    let direction: bool = (report[0] - report[1]) > 0;
+    for idx in 0..report.len()-1 {
+        let diff: i32 = report[idx] - report[idx + 1];
+        if ((diff) > 0) != direction || diff.abs() < 1 || diff.abs() > 3 {
+            return false;
+        }
+    }
+    return true;
+}
