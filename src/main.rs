39b75c2e44aa7d6b59d66fe26a2d07c47c149874
Paul Andert <paulandert@mail.de>
2024-12-04 19:42:04 +0100

    finished day 04 a

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
 
     // days::day_01_a::main(); // to execute a day
     
-    y2024::day_03_a::main();
+    y2024::day_04_a::main();
 
     // cargo rustc -- -Awarnings && ./target/debug/aoc
     // to remove warnings
--- src/y2024/day_04_a.rs
+++ src/y2024/day_04_a.rs
@@ -1,39 +1,103 @@
-extern crate regex;
-
 use std::fs;
-use regex::Regex;
 
 pub fn main() {
-    let contents = fs::read_to_string("./src/y2024/resources/day_03").expect("Should have been able to read the file");
+    let contents = fs::read_to_string("./src/y2024/resources/day_04").expect("Should have been able to read the file");
+    let mut cnt: u32 = 0;
 
-    let regex: Regex = Regex::new(
-        r#"mul\([0-9]{1,3},[0-9]{1,3}\)|do\(\)|don't\(\)"#
-    ).unwrap();
-
-    let mut count = 0;
-    let mut active: bool = true;
-
-    for find in regex.find_iter(contents.as_str()) {
-        let mut operation: String = find.as_str().to_string();
-
-        if operation.contains("do") {
-            if operation.contains("don't") {
-                active = false;
-            }else {
-                active = true;
-            }
-        }
-
-        if active {
-            operation = operation.replace("mul(", "");
-            operation = operation.replace(")", "");
-            let lists: Vec<&str> = operation.split(",").collect::<Vec<&str>>();
-            if lists.len() == 2 {
-                let numbers: Vec<u32> = lists.iter().map(|a| a.parse::<u32>().unwrap()).collect();
-                count += numbers[0] * numbers[1];
-            }
+    let mut matrix: Vec<Vec<char>> = Vec::new();
+    for line in contents.split("\n") {
+        let chars: Vec<char> = line.chars().collect();
+        if chars.len() > 0 {
+            matrix.push(chars);
         }
     }
 
-    println!("C: {}", count);
+    // horizontal
+    for idx in 0..matrix.len() {
+        cnt += count_xmas(matrix[idx].clone());
+    }
+
+    // vertical
+    for idx in 0..matrix[0].len() {
+        let mut line: Vec<char> = Vec::new();
+        for jdx in 0..matrix.len() {
+            line.push(matrix[jdx][idx]);
+        }
+        cnt += count_xmas(line);
+    }
+
+    // diagonally right
+    // x axis
+    for idx in 0..matrix[0].len() {
+        
+        let mut line: Vec<char> = Vec::new();
+        let mut loc_offset: usize = 0;
+
+        while loc_offset < matrix.len() && (idx + loc_offset) < matrix[loc_offset].len() {
+            line.push(matrix[loc_offset][idx + loc_offset]);
+            loc_offset += 1;
+        }
+        cnt += count_xmas(line);
+    }
+    
+    // y axis
+    for idx in 1..matrix.len() {
+        
+        let mut line: Vec<char> = Vec::new();
+        let mut loc_offset: usize = 0;
+        
+        while (idx + loc_offset) < matrix.len() && loc_offset < matrix[loc_offset].len() {
+            line.push(matrix[idx + loc_offset][loc_offset]);
+            loc_offset += 1;
+        }
+        cnt += count_xmas(line);
+    }
+    
+    // diagonally left
+    // x axis
+    for idx in 0..matrix[0].len() {
+        
+        let mut line: Vec<char> = Vec::new();
+        let mut loc_offset: usize = 0;
+        
+        while loc_offset < matrix.len() && (idx as i32 - loc_offset as i32) >= 0 {
+            line.push(matrix[loc_offset][idx - loc_offset]);
+            loc_offset += 1;
+        }
+        cnt += count_xmas(line);
+    }
+    
+    // y axis
+    for idx in 1..matrix.len() {
+        
+        let mut line: Vec<char> = Vec::new();
+        let mut loc_offset: usize = 0;
+        
+        let end = matrix[loc_offset].len() - 1;
+        while idx + loc_offset < matrix.len() && (end as i32 - loc_offset as i32) > 0 {
+            line.push(matrix[idx + loc_offset][end - loc_offset]);
+            loc_offset += 1;
+        }
+        cnt += count_xmas(line);
+    }
+
+    println!("C: {}", cnt);
+
+}
+
+fn count_xmas(line: Vec<char>) -> u32 {
+    let mut cnt = 0;
+
+    let mut idx: usize = 0;
+    while idx < line.len() {
+        if idx + 3 < line.len() && line[idx] == 'X' && line[idx + 1] == 'M' && line[idx + 2] == 'A' && line[idx + 3] == 'S' {
+            cnt += 1;
+        }
+        if idx + 3 < line.len() && line[idx] == 'S' && line[idx + 1] == 'A' && line[idx + 2] == 'M' && line[idx + 3] == 'X' {
+            cnt += 1
+        }
+        idx += 1
+    }
+
+    return cnt;
 }
